c032f8012f5ebd9652675938a470aa7d34936f1b
Yuanqing Wang <wangyq+gh@wangyq.net>
2022-06-19 12:52:15 -0400

    nan_to_num for segment

--- galax/function.py
+++ galax/function.py
@@ -134,9 +134,24 @@ max = partial(ReduceFunction, "max")
 min = partial(ReduceFunction, "min")
 
 segment_sum = jax.ops.segment_sum
-segment_max = jax.ops.segment_max
-segment_min = jax.ops.segment_min
 
+def segment_max(*args, **kwargs):
+    return jnp.nan_to_num(
+        jax.ops.segment_max(*args, **kwargs),
+        nan=0.0,
+        posinf=0.0,
+        neginf=0.0,
+    )
+segment_max.__doc__ == jax.ops.segment_max.__doc__
+
+def segment_min(*args, **kwargs):
+    return jnp.nan_to_num(
+        jax.ops.segment_min(*args, **kwargs),
+        nan=0.0,
+        posinf=0.0,
+        neginf=0.0,
+    )
+segment_min.__doc__ = jax.ops.segment_min.__doc__
 
 def segment_mean(
     data: jnp.ndarray,

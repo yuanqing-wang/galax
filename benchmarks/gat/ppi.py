3bd2e55766ecbb8d7dade0d2af2eb95c71686a64
Yuanqing Wang <wangyq+gh@wangyq.net>
2022-07-07 16:29:49 -0400

    ppi

--- benchmarks/gat/ppi.py
+++ benchmarks/gat/ppi.py
@@ -82,10 +82,15 @@ def run():
     def eval(state, g):
         g = model.apply(state.params, g)
         y_hat = g.ndata['h'][g.is_not_dummy()]
+        _loss = optax.sigmoid_binary_cross_entropy(
+            g.ndata['h'], g.ndata['label'],
+        ).mean()
+        _loss = jnp.where(jnp.expand_dims(g.is_not_dummy(), -1), _loss, 0.0)
+        _loss = _loss.sum() / len(_loss)
         y_hat = 1 * (jax.nn.sigmoid(y_hat) > 0.5)
         y = g.ndata['label'][g.is_not_dummy()]
         accuracy = (y_hat == y).sum() / y.size
-        return accuracy
+        return _loss, accuracy
 
     from galax.nn.utils import EarlyStopping
     early_stopping = EarlyStopping(10)
@@ -94,9 +99,13 @@ def run():
     for _ in tqdm.tqdm(range(1000)):
         for idx, g in enumerate(ds_tr):
             state = step(state, g)
-        accuracy = eval(state, g_vl)
-        print(accuracy)
+        loss_vl, accuracy_vl = eval(state, g_vl)
+        if early_stopping((-accuracy_vl, loss_vl), state.params):
+            state = state.replace(params=early_stopping.params)
+            break
 
+    _, accuracy = eval(state, g_te)
+    print(accuracy)
 
 if __name__ == "__main__":
     import argparse

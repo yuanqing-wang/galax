b6c09cb69a2ee80ec2dd0866ca7cbde117953c75
Yuanqing Wang <wangyq+gh@wangyq.net>
2022-07-08 14:41:00 -0400

    start graph sage

---
+++ galax/nn/zoo/graphsage.py
@@ -0,0 +1,43 @@
+"""`GraphSAGE <https://cs.stanford.edu/~jure/pubs/graphsage-nips17.pdf>`__"""
+
+from typing import Callable, Optional
+import jax
+import jax.numpy as jnp
+from flax import linen as nn
+from ... import function as fn
+from ..module import Module
+
+class GraphSAGE(Module):
+    r"""GraphSAGE layer from `Inductive Representation Learning on
+    Large Graphs <https://arxiv.org/pdf/1706.02216.pdf>`__
+    .. math::
+        h_{\mathcal{N}(i)}^{(l+1)} &= \mathrm{aggregate}
+        \left(\{h_{j}^{l}, \forall j \in \mathcal{N}(i) \}\right)
+        h_{i}^{(l+1)} &= \sigma \left(W \cdot \mathrm{concat}
+        (h_{i}^{l}, h_{\mathcal{N}(i)}^{l+1}) \right)
+        h_{i}^{(l+1)} &= \mathrm{norm}(h_{i}^{(l+1)})
+
+    Parameters
+    ----------
+    in_feats : int, or pair of ints
+        Input feature size; i.e, the number of dimensions of :math:`h_i^{(l)}`.
+        If aggregator type is ``gcn``, the feature size of source and destination
+        nodes are required to be the same.
+    out_feats : int
+        Output feature size; i.e, the number of dimensions of :math:`h_i^{(l+1)}`.
+    aggregator_type : str
+        Aggregator type to use (``mean``, ``gcn``, ``pool``, ``lstm``).
+    feat_drop : float
+        Dropout rate on features, default: ``0``.
+    use_bias : bool
+        If True, adds a learnable bias to the output. Default: ``True``.
+    norm : callable activation function/layer or None, optional
+        If not None, applies normalization to the updated node features.
+    activation : callable activation function/layer or None, optional
+        If not None, applies an activation function to the updated node features.
+        Default: ``None``.
+    """
+    features: int
+    aggregator_type: str = "mean"
+    use_bias: bool = True
+    activation : Optional[Callable] = None
